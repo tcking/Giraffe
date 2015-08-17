3c68f0b239155cfeafa669142fa4c164fce2a21b
汤超 <tangchao@chinasofti.com>
2015-08-17 17:08:39 +0800

    add Listeners

---
+++ app/src/main/java/com/github/tcking/giraffe/helper/Listeners.java
@@ -0,0 +1,34 @@
+package com.github.tcking.giraffe.helper;
+
+import android.text.Editable;
+
+/**
+ * <pre>
+ * 一些Listeners的适配，do nothing，简化代码，例如想要增加一个TextWatcher，而只对其中的一个afterTextChanged感兴趣，
+ * new Listeners.TextWatcher() {
+ *      @Override public void afterTextChanged(Editable s) {
+ *          //do something
+ *      }
+ * };
+ * </pre>
+ * Created by tc(mytcking@gmail.com) on 15/8/17.
+ */
+public class Listeners {
+    public static class TextWatcher implements android.text.TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
+}
